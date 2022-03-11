09031f341d386e85067cf04c56a93280e58c4894
kedarbhusari <kedarbhusari@gmail.com>
2022-03-11 15:33:48 -0600

    initial commit

---
+++ src/test/java/com/example/demoWorld/DemoWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
